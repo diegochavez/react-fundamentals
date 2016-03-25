153c3b581458b95cf7ba0780f4c67212415bd60d
Diego Chavez <diego@volumens.com>
2016-03-25 10:05:30 -0600

    Added example of relational components

--- state_basics/App.js
+++ state_basics/App.js
@@ -5,6 +5,7 @@ class App extends React.Component {
 	constructor(){
 		super();
 		this.state = { text : 'this is a text'}
+		this.update = this.update.bind(this)
 	}
 	update(e){
 		this.setState({text: e.target.value})
@@ -12,13 +13,22 @@ class App extends React.Component {
 	render(){
 		return (
 			<div>
-				<input type="text"
-					onChange={this.update.bind(this)}
-					/>
-				<h1>{this.state.text}</h1>
+				<Widget text={this.state.text} update={this.update} />
 			</div>
 		)
 	}
+}
+
+//Stateless Component
+const Widget = (props) => {
+		return (
+			<div>
+				<input type="text"
+					onChange={props.update}
+					/>
+				<h1>{props.text}</h1>
+			</div>
+		)
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'))
