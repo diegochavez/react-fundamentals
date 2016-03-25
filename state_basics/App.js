9c8ef613e0a88d33707ff3da510c97b9049315a9
Diego Chavez <diego@volumens.com>
2016-03-25 15:00:39 -0600

    Accessing Child Properties

--- state_basics/App.js
+++ state_basics/App.js
@@ -2,52 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	constructor(){
-		super();
-		this.state = {
-			red: 0,
-			green: 0,
-			blue: 0
+		render(){
+				return <Button>I <Heart/> React</Button>
 		}
-		this.update = this.update.bind(this)
 	}
-	update(e){
-		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-			green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-		})
-	}
+
+class Button extends React.Component {
 	render(){
-		return (
-			<div>
-				<Slider ref="red" update={this.update} />
-				{this.state.red}
-				<br/>
-				<Slider ref="green" update={this.update} />
-				{this.state.green}
-				<br/>
-				<Slider ref="blue" update={this.update} />
-				{this.state.blue}
-				<br/>
-			</div>
-		)
+		return <button>{this.props.children}</button>
 	}
 }
 
-class Slider extends React.Component{
-	render(){
-		return (
-			<div>
-				<input ref="inp" type="range"
-					min="0"
-					max="255"
-					onChange={this.props.update}
-					/>
-			</div>
-		)
-	}
-}
-
+const Heart = () => <span className="fa fa-heart"></span>
 
 ReactDOM.render(<App/>, document.getElementById('app'))
