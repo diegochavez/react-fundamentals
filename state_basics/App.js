99054b9a949238f809b59fa354c2009a4ff3fa92
Diego Chavez <diego@volumens.com>
2016-03-26 22:04:17 -0600

    To make more composable React components, you ca define common APIs for similar component types.

--- state_basics/App.js
+++ state_basics/App.js
@@ -6,48 +6,68 @@ class App extends React.Component {
 		super();
 		this.state = {
 			red: 0,
-			green: 0,
-			blue: 0
 		}
 		this.update = this.update.bind(this)
 	}
 	update(e){
 		this.setState({
 			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-			green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
 		})
 	}
 	render(){
 		return (
 			<div>
-				<Slider ref="red" update={this.update} />
-				{this.state.red}
-				<br/>
-				<Slider ref="green" update={this.update} />
-				{this.state.green}
-				<br/>
-				<Slider ref="blue" update={this.update} />
-				{this.state.blue}
-				<br/>
+				<NumInput ref="red"
+					min={0}
+					max={255}
+					step={0.01}
+					val={+this.state.red}
+					type="range"
+					label="Red"
+					update={this.update}
+				/>
 			</div>
 		)
 	}
 }
 
-class Slider extends React.Component{
+class NumInput extends React.Component{
 	render(){
+		let label = this.props.label !== '' ?
+			<label>{this.props.label} - {this.props.val}</label> : ''
 		return (
 			<div>
-				<input ref="inp" type="range"
-					min="0"
-					max="255"
+				<input ref="inp"
+					type={this.props.type}
+					min={this.props.min}
+					max={this.props.max}
+					step={this.props.step}
+					defaultValue={this.props.val}
 					onChange={this.props.update}
 					/>
+					{label}
 			</div>
 		)
 	}
 }
 
+NumInput.propTypes = {
+	min: React.PropTypes.number,
+	max: React.PropTypes.number,
+	step: React.PropTypes.number,
+	val: React.PropTypes.number,
+	label: React.PropTypes.string,
+	update: React.PropTypes.func.isRequired,
+	type: React.PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+	min: 0,
+	max: 0,
+	step: 1,
+	val: 0,
+	label: '',
+	type: 'range'
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'))
